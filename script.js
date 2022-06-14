3049bc776897e389e99adc84b8be96e98fe3758e
kavalosc <59745971+kavalosc@users.noreply.github.com>
2022-06-13 23:30:25 -0700

    updated

---
+++ script.js
@@ -0,0 +1,6 @@
+function changeToDarkMode(){
+    document.documentElement.style.setProperty('--background', "#151515");
+    document.documentElement.style.setProperty('--card-background', "#15171A");
+    document.documentElement.style.setProperty('--text-color', "white");
+}
+
