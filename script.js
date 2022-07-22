4f5ab8be4aad48f16d97f008c669d3407742db48
kavalosc <59745971+kavalosc@users.noreply.github.com>
2022-07-22 13:47:34 -0700

    updated

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
-function changeToDarkMode(){
-    document.documentElement.style.setProperty('--background', "#151515");
-    document.documentElement.style.setProperty('--card-background', "#15171A");
-    document.documentElement.style.setProperty('--text-color', "white");
-}
+let darkTheme = JSON.parse(localStorage.getItem('theme')) ?? false;
 
+function changeToDarkMode(){
+
+    if (darkTheme) {
+        document.documentElement.style.setProperty('--background-color', "#121212");
+        document.documentElement.style.setProperty('--blue', "#BB86FC");
+        document.documentElement.style.setProperty('--red', "#ff8775");
+        document.documentElement.style.setProperty('--yellow', "#FBDF8C");
+        document.documentElement.style.setProperty('--light', '#44445e');
+        document.documentElement.style.setProperty('--color', "#F1F1FD");
+        document.documentElement.style.setProperty('--inactive', "#535370");
+
+    } else {
+        document.documentElement.style.setProperty('--background-color', "white");
+        document.documentElement.style.setProperty('--blue', "#7879F1");
+        document.documentElement.style.setProperty('--red', "#FF715B");
+        document.documentElement.style.setProperty('--yellow', "#F9CB40");
+        document.documentElement.style.setProperty('--light', '#F9F5FF');
+        document.documentElement.style.setProperty('--color', "#28262C");
+        document.documentElement.style.setProperty('--inactive', "#afafaf");
+
+    };
+    darkTheme = !darkTheme;
+    localStorage.setItem('theme', darkTheme);
+
+
+}
