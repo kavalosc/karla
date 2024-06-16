ef743bca136e17b1454f31048d9f23dd65708572
kavalosc <59745971+kavalosc@users.noreply.github.com>
2024-06-15 18:43:35 -0700

    updating

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-$(document).ready(function(){
-    $('.card-container').click(function(){
-      $(this).toggleClass("spread");
-    });
-  });
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopNav");
+  if (x.className === "navText") {
+    x.className += " responsive";
+  } else {
+    x.className = "navText";
+  }
+}
+
